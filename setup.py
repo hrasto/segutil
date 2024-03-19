2e058e6cd54071dc3bfa7f95af6b4368ea5fe090
Rastislav Hronsky <hronskyr@gmail.com>
2024-03-19 12:04:37 +0100

    rename update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="segmenters",
+    name="segutil",
     version="0.1.7",
     author="Rastislav Hronsky",
     author_email="hronskyr@gmail.com",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/hrasto/segmenters",
     project_urls={
-        "Bug Tracker": "https://github.com/hrasto/segmenters/issues",
+        "Bug Tracker": "https://github.com/hrasto/segutil/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -25,7 +25,7 @@ setuptools.setup(
     python_requires=">=3.6",
     include_package_data=True,
     package_data={
-        "segmenters": ["data/*"],
+        "segutil": ["data/*"],
     },
     install_requires=[
         'pandas',
